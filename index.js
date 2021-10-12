5b6b2b99d6e2de8dd3ac27821190ce827eb082d3
naberin <aberinnj@gmail.com>
2021-10-12 17:10:00 -0500

    Added vue CDN and app

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function setMenuToggleListener() {
-   let menuToggle = document.getElementById("nav-menu-toggle");
-   let menu = document.getElementById("nav-menu-links");
+var app = new Vue({
+   el: "#personal-app",
+   data: {
+      fullname: {first: "Norman", last: "Aberin"}
+   },
+   methods: {
+      setMenuToggleListener: function () {
+         let menuToggle = document.getElementById("nav-menu-toggle");
+         let menu = document.getElementById("nav-menu-links");
+      
+         menuToggle.addEventListener('click', function() {
+            menu.classList.toggle('menu-is-active');
+         });
+      }
+      
+   },
+   mounted() {
+      this.setMenuToggleListener();
+   }
 
-   menuToggle.addEventListener('click', function() {
-      menu.classList.toggle('menu-is-active');
-   });
-}
-
-setMenuToggleListener();
+})
