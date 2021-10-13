702ecd292eb9b62092947b16380705e9ddbe657b
naberin <aberinnj@gmail.com>
2021-10-13 11:51:10 -0500

    Integrated App Navigation

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ let Home = Vue.component('Home', {
     <section class="page">
         <app-hero :info="info"></app-hero>
         <app-about :info="info"></app-about>
-
+        <app-navigation></app-navigation>
     </section>
     `
 });
