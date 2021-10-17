ffcf7b22a7d83a92a9424f69a63d33ca9e1423fa
naberin <aberinnj@gmail.com>
2021-10-17 00:03:00 -0500

    Fixed navigation warning with no keys

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,6 @@
 Vue.component('app-navigation', {
     methods: {
         isCurrent(path) {
-            console.log(this.$route);
             return this.$route.name === path;
         }
     },
@@ -9,16 +8,16 @@ Vue.component('app-navigation', {
         return {
             links: [
                 {id: 1, path: 'home', name: "Home", icon: `<i class="fas fa-home"></i>`, to: "/"},
-                {id: 1, path: 'projects', name: "My Projects", icon: `<i class="fas fa-shapes"></i>`, to: "/projects"},
-                {id: 2, path: 'skills', name: "My Skills", icon: `<i class="fab fa-js-square"></i>`, to: "/skills"},
-                {id: 3, path: 'interests', name: "Interests", icon: `<i class="fas fa-star"></i>`, to: "/interests"},
-                {id: 4, path: 'themes', name: "Themes", icon: `<i class="fas fa-palette"></i>`, to: "/themes"}
+                {id: 2, path: 'projects', name: "My Projects", icon: `<i class="fas fa-shapes"></i>`, to: "/projects"},
+                {id: 3, path: 'skills', name: "My Skills", icon: `<i class="fab fa-js-square"></i>`, to: "/skills"},
+                {id: 4, path: 'interests', name: "Interests", icon: `<i class="fas fa-star"></i>`, to: "/interests"},
+                {id: 5, path: 'themes', name: "Themes", icon: `<i class="fas fa-palette"></i>`, to: "/themes"}
             ]
         }
     },
     template:
     `<nav class="flex flex-row flex-justify-center app-navigation">
-        <router-link v-for="link in links" class="flex flex-row app-button" :to="link.to">
+        <router-link v-for="link in links" :key="link.id" class="flex flex-row app-button" :to="link.to">
             <span class="icon" v-html="link.icon" :title="link.name"></span>
             <span class="title" v-if="isCurrent(link.path)">{{link.name}}</span>
         </router-link>
