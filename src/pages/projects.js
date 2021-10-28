87c987d5776333a2ab5ac87f06c1f4f46c0e9759
naberin <aberinnj@gmail.com>
2021-10-28 00:58:17 -0500

    Added Project Form and Styling

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,33 @@
 let Projects = Vue.component('Projects', {
     props: {
-        info: Object
+        projects: Array
+    },
+    data() {
+        return {
+            projectName: "",
+            projectDetails: ""
+        }
+    },
     },
     template: `
-    <section class="page">
+    <section class="page app-projects">
         <div class="container">
             <mac-window>
-            
+                <div class="list-app flex flex-row flexible">
+                    <div class="projects-form flex-grow-4 flex flex-col ">
+                        <div class="message flex flex-col centered">
+                            <span class="title"><i class="fas fa-shapes"></i> Project List</span>
+                            <span class="description">The list may be currently <strong>unavailable</strong>. Temporarily, here is a form and list for creating and managing Project entries.</span>
+                        </div>
+                        <div class="form-controls flex flex-col centered">
+                            <input placeholder="Project Name" v-model="projectName">
+                            <textarea placeholder="Details" v-model="projectDetails"></textarea>
+                            <button class="button add"><i class="fas fa-plus"></i> Add</button>
+                        </div>
+                        
+                    </div>  
+                    <div class="flex-grow-6"></div>
+                </div>
             </mac-window>
         </div>
     </section>
