bd29c1c2bff64ed9166347568a948f016651fd71
naberin <aberinnj@gmail.com>
2021-10-29 17:19:54 -0500

    Fixed skills on mobile devices

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -6,11 +6,11 @@ let Skills = Vue.component('Skills', {
     <section class="page app-skills">    
 
         <div class="skills-container">
-            <div class="container flex flex-row">
-                <div v-for="(skills, category) in info.skills" class="flex flex-col col-2">
+            <div class="container flex flex-row flexible">
+                <div v-for="(skills, category) in info.skills" class="flex flex-col flexible flex-grow-1">
                     <span class="marker centered"></span>
                     <div class="category center">{{category}}</div>
-                    <ul class="flex flex-col">
+                    <ul class="flex flex-col flexible">
                         <li v-for="skill in skills" class="skill center">{{skill}}</li>
                     </ul>
                 </div>
