ef5f34319a4da500ce2ffc6b2c6cde967b3b97a6
naberin <aberinnj@gmail.com>
2021-10-28 21:05:46 -0500

    Added contact-links

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,13 @@ let Home = Vue.component('Home', {
         info: Object
     },
     template: `
-    <section class="page">
+    <section class="page app-home">
         <app-hero :info="info"></app-hero>
         <app-about :info="info"></app-about>
+        <div class="contact-links flex flex-row flex-justify-center">
+            <a href="https://linkedin.com/in/norman-aberin" target="_blank" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/naberin" target="_blank" title="GitHub"><i class="fab fa-github"></i></a>
+        </div>
     </section>
     `
 });
