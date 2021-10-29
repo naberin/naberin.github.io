291e417bbc50e954c80c8ea18bf9950f01838652
naberin <aberinnj@gmail.com>
2021-10-29 01:16:11 -0500

    Added skills page and styling

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,7 +3,22 @@ let Skills = Vue.component('Skills', {
         info: Object
     },
     template: `
-    <section class="page">
+    <section class="page app-skills">    
+
+        <div class="skills-container">
+            <div class="container flex flex-row">
+                <div v-for="(skills, category) in info.skills" class="flex flex-col col-2">
+                    <span class="marker centered"></span>
+                    <div class="category center">{{category}}</div>
+                    <ul class="flex flex-col">
+                        <li v-for="skill in skills" class="skill center">{{skill}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
     </section>
     `
 });
