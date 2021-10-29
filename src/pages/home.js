9262f85de01bd905ff1f1e8f84adee49f7e9a52c
naberin <aberinnj@gmail.com>
2021-10-29 16:59:02 -0500

    Fixed contact-links covering text with absolute

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,19 @@ let Home = Vue.component('Home', {
         info: Object
     },
     template: `
-    <section class="page app-home">
-        <app-hero :info="info"></app-hero>
-        <app-about :info="info"></app-about>
-        <div class="contact-links flex flex-row flex-justify-center">
-            <a href="https://linkedin.com/in/norman-aberin" target="_blank" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://github.com/naberin" target="_blank" title="GitHub"><i class="fab fa-github"></i></a>
+    <section class="page app-home flex flex-col">
+        <div class="flex-grow-3">        
+            <app-hero :info="info" class="flex-grow-2"> </app-hero>
+            <app-about :info="info" class="flex-grow-2"></app-about>
         </div>
+
+        <div class="flex flex-col flex-justify-end flex-grow-7">
+            <div class="contact-links flex flex-row flex-justify-center">
+                <a href="https://linkedin.com/in/norman-aberin" target="_blank" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/naberin" target="_blank" title="GitHub"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+
     </section>
     `
 });
