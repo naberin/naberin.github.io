c177cc114c8278fe770bc6931a6e6bb468b41625
naberin <aberinnj@gmail.com>
2021-12-01 23:25:08 -0600

    Updated navigation and links styling

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,15 +9,18 @@ Vue.component('app-navigation', {
             links: [
                 {id: 1, path: 'about', name: "about", icon: ``, to: "/about"},
                 {id: 2, path: 'projects', name: "projects", icon: ``, to: "/projects"},
-                {id: 3, path: 'resume', name: "resume", icon: ``, to: "/themes"}
+                {id: 3, path: null, name: "resume", icon: ``, to: "https://drive.google.com/file/d/1h3RAfZbvLuMhZs11tN_m4tfJ-nxZSsSe/view?usp=sharing"}
             ]
         }
     },
     template:
     `<nav class="flex flex-row flex-justify-center app-navigation">
-        <router-link v-for="link in links" :key="link.id" :class="['flex flex-row link', isCurrent(link.name)]" :to="link.to">
-            <span class="title" >{{link.name}}</span>
-        </router-link>
+        <span v-for="link in links" :key="link.id" :class="['flex flex-row link', isCurrent(link.name)]">
+            <router-link v-if="link.path" :to="link.to">
+                <span class="title" >{{link.name}}</span>
+            </router-link>
+            <a v-else :href="link.to" :title="link.name" target="_blank">{{link.name}}</a>
+            </span>
     </nav>
     `
 });
